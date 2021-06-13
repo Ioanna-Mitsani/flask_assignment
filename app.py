a4b29c44c96dd70dcc83076e553a13cbb1e659b8
Ioanna-Mitsani <13-13099@saeinstitute.edu>
2021-06-13 03:55:04 +0300

    profile authentication, queries etc.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 # Import here the elements needed from libraries or whole libraries
 from flask import Flask, render_template, request, session, redirect, flash, url_for, g
 # redirect, flash, request abort
@@ -10,21 +9,22 @@ import sqlite3
 app = Flask(__name__)
 
 # Secret key declaration to create browser session
-app.secret_key = '5613a0e127ad32e2642d975d23932d7a786107ab79061b11'
+app.secret_key = environ.get('SECRET_KEY', 'random1234')
 
-DATABASE_PATH = Path(__file__).parent / 'data/flask_project.db'
+DATABASE_PATH = Path(__file__).parent / environ.get("DB_PATH", 'data/flask_project.db')
 
 
+# Function that initializes the connection to Database
 def get_conn():
-    if 'conn' not in g:     # hasattr(g, 'conn')
-        app.logger.debug(f"» New Connection requested from endpoint '{request.endpoint}'")
+    if not hasattr(g, 'conn'):
+        app.logger.debug(f"New Connection requested from endpoint '{request.endpoint}'")
         conn = sqlite3.connect(DATABASE_PATH)
         conn.row_factory = sqlite3.Row
-        g.conn = conn       # setattr(g, 'conn', conn)
-
+        setattr(g, 'conn', conn)
     return g.conn
 
 
+# Function executed before every request
 @app.before_request
 def get_users():
     # Quick return if request comes from static asset
@@ -43,6 +43,7 @@ def get_users():
         setattr(g, 'users', users)
 
 
+# Function that destroys connection to database
 @app.teardown_request
 def teardown_request(e):
     '''
@@ -72,19 +73,22 @@ def homepage():
     return render_template("index.html")  # Renders index.html
 
 
+# Dashboard router
 @app.get('/dashboard')
 def dashboard():
-    return render_template("dashboard.html")
+    if session['username']:
+        return render_template("dashboard.html")
+    else:
+        return render_template('page-403.html')
 
 
 # Login form page 'GET' router
 @app.get('/login')
 def login_page():
-    if session['username'] is None:
-        return render_template("login.html")  # Renders login.html
-    else:
-        flash('You are already logged in!')
-        return redirect(url_for('profile'))
+    if session:
+        return redirect(url_for('dashboard'))
+
+    return render_template('login.html')  # Renders login.html
 
 
 # Login form 'POST' method handling
@@ -94,28 +98,41 @@ def login():
     form_password = request.form.get('password')  # Registering the password input
 
     cur = get_conn().cursor()
-    users = cur.execute(
+    user = cur.execute(
             '''
-            SELECT [uid], [username], [password], [email], [f_name], [l_name], [address],
-            [city], [country], [postal_code], [about] FROM [user]
-            '''
-        ).fetchall()
-    setattr(g, 'users', users)
+            SELECT [uid], [username], [password]
+            FROM [user]
+            WHERE [username] = :username AND [password] = :password
+            ''',
+            {'username': form_username, 'password': form_password}
+        ).fetchone()
+    setattr(g, 'user', user)
 
-    if form_username == users[0]['username'] and form_password == users[0]['password']:  # Credentials check with database
-        session['username'] = form_username  # If credentials are valid, are registered to cookie
-        session['password'] = form_password
+    if form_username == user['username'] and form_password == user['password']:  # Credentials check with the ones in database
+        session['username'] = user['username']  # If credentials are valid, are registered to session obj
+        session['password'] = user['password']
         return redirect(url_for('dashboard'))
     else:
-        return render_template('page-403.html'), 403  # Alternatively: abort(403)
+        flash('Please enter the correct credentials!')
+        return redirect(url_for('login_page'))
 
 
-@app.get('/profile')
-def profile():
-    # if session['username'] and session['password'] is not None:
-    return render_template("profile.html")
-    # else:
-    # abort(403)
+@app.route('/profile/<username>')
+def profile(username):
+    cur = get_conn().cursor()
+    user = cur.execute(
+        '''
+        SELECT *
+        FROM [user]
+        WHERE [username] = :username
+        ''',
+        {'username': username}
+    ).fetchone()
+
+    if user is None:
+        return render_template('page-404.html')
+
+    return render_template('profile.html', user=user)
 
 
 @app.get('/logout')
